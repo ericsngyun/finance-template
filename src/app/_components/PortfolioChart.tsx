5246d7ba3fcee83b21b3758fca4affc13fdbd30c
Eric Yun <yunseric@gmail.com>
2025-02-17 17:38:34 -0800

    fix: add changes to theme-provider.tsx to fix hydration error

---
+++ src/app/_components/PortfolioChart.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const PortfolioChart = () => {
+  return (
+    <div>PortfolioChart</div>
+  )
+}
+
+export default PortfolioChart
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex justify-center place-">
-
+    <div className="flex justify-center place-items-center text-primary">
+      hello
     </div>
   );
 }
